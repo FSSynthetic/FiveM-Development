fc6162849a7af05a16613f01606a5c39bd2cd8db
FSSynthetic <Benjamin.Leverrier@pm.me>
2023-08-30 12:59:11 +0000

    Delete FSscoreboard-main/FSscoreboard-main directory

--- FSscoreboard-main/FSscoreboard-main/config.lua
+++
@@ -1,8 +0,0 @@
-Config = {
-    icon = "https://www.vhv.rs/dpng/d/228-2280680_yourlogo-icon-your-logo-goes-here-hd-png.png", -- This is the icon that will appear on the top right of the scoreboard
-    ServerName = "Your Server Name", -- This is the server name that will appear on the top left of the scoreboard
-    LinkOne = "YourServer.com", -- This is the top small text to the right of the icon (P.S. doesn't need to be a link, can be whatever you want)
-    LinkTwo = "discord.gg/YourServer", -- This is the bottom small text to the right of the icon (P.S. doesn't need to be a link, can be whatever you want)
-    MaxPlayers = 64, -- The maximum amount of players that can connect to your server
-    KeyBind = 243, -- The keybind to open and close the scoreboard (Default is ~). List of key codes can be found here: https://docs.fivem.net/docs/game-references/controls/
-}
